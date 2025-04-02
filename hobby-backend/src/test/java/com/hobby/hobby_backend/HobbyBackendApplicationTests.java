d14bd3191a5fb10e029ea499f985fc2f3ecaede0
Don-tresor05 <dontresor922@gmail.com>
2025-04-03 00:39:37 +0200

    First Commit in Hobby navigator project

---
+++ hobby-backend/src/test/java/com/hobby/hobby_backend/HobbyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobby.hobby_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
